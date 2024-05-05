0d89423d31141c66d6954db70b1eed1685f4b47e
ramazanerduran <ramazan.erduran@outlook.com.tr>
2024-05-05 13:22:33 +0300

    streamlit main runner app

---
+++ Streamlit/app.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import numpy as np
+
+import streamlit as st
+import streamlit_option_menu as sm
+
+import plotly.express as px
+
+import warnings
+warnings.filterwarnings("ignore")
