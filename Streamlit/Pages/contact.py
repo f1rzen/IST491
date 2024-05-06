2c2c1231ec0d3abd47d30fe5c507c33bf2a70e0a
İlkay <ilkaybaytar@aol.com>
2024-05-06 19:17:28 +0300

    gereksiz css kaldırıldı, picoCSS eklendi (form için)

--- Streamlit/Pages/contact.py
+++ Streamlit/Pages/contact.py
@@ -2,25 +2,21 @@ import streamlit as st
 
 def show_contact_page():
     
-    html_path = '../Streamlit/ContactPage/index.html'
-    css_path = '../Streamlit/ContactPage/style.css'
+    html_path = './ContactPage/index.html'
     
-    def get_contact_page(css_path, html_path):
-        with open(css_path, 'r', encoding='utf-8') as f:
-            contact_page_css = f.read()
+    def get_contact_page(html_path):
         
         with open(html_path, 'r', encoding='utf-8') as f:
             contact_page = f.read()
 
-        return contact_page_css, contact_page
+        return contact_page
 
-    contact_page, contact_css_file = get_contact_page(html_path, css_path)
-
+    contact_page= get_contact_page(html_path)
     st.markdown(
         f"""
-        <style>
-            {contact_css_file}
-        </style>
+        ## Contact us
+
+        Contact us for your questions.
         {contact_page}
         """,
         unsafe_allow_html=True
